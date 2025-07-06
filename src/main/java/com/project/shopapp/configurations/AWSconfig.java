5f310d8d4b03e85bcad42a9f3c8d936f198ec85a
tranthaibinh1998 <tranthaibinh1998@gmail.com>
2025-07-06 11:21:45 +0900

    feat: add configuration

--- src/main/java/com/project/shopapp/configurations/AWSconfig.java
+++ src/main/java/com/project/shopapp/configurations/AWSconfig.java
@@ -1,9 +1,11 @@
 package com.project.shopapp.configurations;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+@Configuration
 public class AWSconfig {
     @Bean
     public S3Client s3Client() {
