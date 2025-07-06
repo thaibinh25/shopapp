e5f557429968b00631e157beda09986916225d98
tranthaibinh1998 <tranthaibinh1998@gmail.com>
2025-07-06 11:26:27 +0900

    feat: add @service,@@RequiredArgsConstructor

--- src/main/java/com/project/shopapp/services/S3Service.java
+++ src/main/java/com/project/shopapp/services/S3Service.java
@@ -1,4 +1,5 @@
 package com.project.shopapp.services;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,16 +9,14 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import java.io.IOException;
 import java.util.UUID;
 
-
+@Service
+@RequiredArgsConstructor
 public class S3Service {
     private final S3Client s3Client;
 
     @Value("${aws.s3.bucket}")
     private String bucket;
 
-    public S3Service(S3Client s3Client) {
-        this.s3Client = s3Client;
-    }
 
     public String uploadFile(MultipartFile file) throws IOException {
         String key = "products/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
