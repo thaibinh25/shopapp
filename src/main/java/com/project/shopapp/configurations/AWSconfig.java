163589457c73a075a9f997632f12ade99dbc1c64
tranthaibinh1998 <tranthaibinh1998@gmail.com>
2025-07-06 11:17:38 +0900

    feat: add AWS S3 config with EnvironmentVariableCredentialsProvider

--- src/main/java/com/project/shopapp/configurations/AWSconfig.java
+++ src/main/java/com/project/shopapp/configurations/AWSconfig.java
@@ -1,8 +1,6 @@
 package com.project.shopapp.configurations;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -10,9 +8,8 @@ public class AWSconfig {
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.AP_NORTHEAST_1) // ví dụ Tokyo
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("ACCESS_KEY", "SECRET_KEY")))
+                .region(Region.AP_SOUTHEAST_2) // ví dụ Tokyo
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
     }
 }
