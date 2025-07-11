3c13d228cc2c50908aca8823fc97fd629ecee390
tranthaibinh1998 <tranthaibinh1998@gmail.com>
2025-07-11 23:08:53 +0900

    chuẩn hoá tên file ảnh lưu trên s3

--- src/main/java/com/project/shopapp/services/S3Service.java
+++ src/main/java/com/project/shopapp/services/S3Service.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
+import java.text.Normalizer;
 import java.util.UUID;
 
 @Service
@@ -19,7 +20,15 @@ public class S3Service {
 
 
     public String uploadFile(MultipartFile file) throws IOException {
-        String key = "products/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+        // 1. Lấy tên file gốc
+        String originalFileName = file.getOriginalFilename();
+
+        // 2. Chuẩn hóa tên file: xóa dấu tiếng Việt, khoảng trắng, kí tự đặc biệt
+        String normalizedFileName = normalizeFileName(originalFileName);
+
+
+        String key = "products/" + UUID.randomUUID() + "_" + normalizedFileName;
         PutObjectRequest request = PutObjectRequest.builder()
                 .bucket(bucket)
                 .key(key)
@@ -31,6 +40,13 @@ public class S3Service {
                 file.getInputStream(), file.getSize()));
 
         return "https://" + bucket + ".s3.amazonaws.com/" + key;
+    }
+
+    // Hàm chuẩn hóa tên file
+    private String normalizeFileName(String fileName) {
+        String normalized = Normalizer.normalize(fileName, Normalizer.Form.NFD);
+        String withoutDiacritics = normalized.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+        return withoutDiacritics.replaceAll("[^a-zA-Z0-9._-]", "_");
     }
 
 }
